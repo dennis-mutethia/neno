ea900ffd683bdc794289ec38620d206f88cefcbe
Dennis Muga <dennis.mutethia1@accesscfa.com>
2024-12-17 13:17:34 +0300

    all changes

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 import os
 from dotenv import load_dotenv
-from flask import Flask, redirect, url_for
-
-from utils.home import Home
+from flask import Flask, redirect, render_template, url_for
 
 load_dotenv()
 
@@ -16,7 +14,35 @@ def index():
 
 @app.route('/home', methods=['GET'])
 def home(): 
-    return Home()() 
+    return render_template('home.html', page='home')
+
+@app.route('/events', methods=['GET'])
+def events(): 
+    return render_template('events.html', page='events')
+
+@app.route('/live', methods=['GET'])
+def live(): 
+    return render_template('live.html', page='live')
+
+@app.route('/top-videos', methods=['GET'])
+def top_videos(): 
+    return render_template('videos/top.html', page='top_videos')
+
+@app.route('/sermons', methods=['GET'])
+def sermons(): 
+    return render_template('videos/sermons.html', page='sermons')
+
+@app.route('/testimonies', methods=['GET'])
+def testimonies(): 
+    return render_template('videos/testimonies.html', page='testimonies')
+
+@app.route('/praise-and-worship', methods=['GET'])
+def praise_and_worship(): 
+    return render_template('videos/top.html', page='praise_and_worship')
+
+@app.route('/outreach', methods=['GET'])
+def outreach(): 
+    return render_template('videos/outreach.html', page='outreach')
 
 if __name__ == '__main__':
     debug_mode = os.getenv('IS_DEBUG', 'False') in ['True', '1', 't']
--- utils/home.py
+++
@@ -1,9 +0,0 @@
-
-from flask import render_template
-
-class Home():
-    def __init__(self): 
-        pass
-    
-    def __call__(self):
-        return render_template('home.html', page='home')
